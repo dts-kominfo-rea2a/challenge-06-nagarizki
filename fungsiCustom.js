aaeee01a2e9f0a396b05fe5eab974ec99dc12ae3
unknown <akuakusaya@gmail.com>
2022-06-12 23:01:38 +0700

    challenge js callback part 2

--- fungsiCustom.js
+++ fungsiCustom.js
@@ -1,5 +1,5 @@
 // TODO: import module bila dibutuhkan di sini
-
+const fs = require("fs");
 // ! JANGAN DIMODIFIKASI
 let file1 = "./data1.json";
 let file2 = "./data2.json";
@@ -18,7 +18,38 @@ let modifyFile3 = (val) => {
 
 // TODO: Kerjakan bacaData
 // gunakan variabel file1, file2, dan file3
-const bacaData = null;
+const bacaData = (fnCallback) => {
+  let hasilData = [];
+  fs.readFile(file1, "utf8", (err, data) => {
+    if (err) {
+      return fnCallback(err, null);
+    }
+
+    let parseJson = JSON.parse(data);
+    let kataData = parseJson.message.split(" ");
+    hasilData.push(kataData[1]);
+    fs.readFile(file2, "utf-8", (err, data) => {
+      if (err) {
+        return fnCallback(err, null);
+      }
+
+      let parseJson2 = JSON.parse(data);
+      hasilData.push(parseJson2[0].message.split("halo ").join(""));
+
+      fs.readFile(file3, "utf-8", (err, data) => {
+      if (err) {
+        return fnCallback(err, null);
+      }
+
+      let parseJson3 = JSON.parse(data);
+      hasilData.push(parseJson3[0]["data"].message.split("halo ").join(""));
+      fnCallback(null, hasilData);
+      });
+    });
+  });
+};
+
+
 
 // ! JANGAN DIMODIFIKASI
 module.exports = {
